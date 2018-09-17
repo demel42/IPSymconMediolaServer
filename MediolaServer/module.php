5c9ffd5a03f3be0712539e09ab06dd3841149247
demel42 <demel42@web.de>
2018-09-17 17:38:32 +0200

    initial revision

---
+++ MediolaServer/module.php
@@ -0,0 +1,115 @@
+<?php
+
+require_once __DIR__ . '/../libs/common.php';  // globale Funktionen
+
+if (!defined('vtBoolean')) {
+    define('vtBoolean', 0);
+    define('vtInteger', 1);
+    define('vtFloat', 2);
+    define('vtString', 3);
+    define('vtArray', 8);
+    define('vtObject', 9);
+}
+
+class MediolaServer extends IPSModule
+{
+    use MediolaServerCommon;
+
+    public function Create()
+    {
+        parent::Create();
+
+        $this->RegisterPropertyString('hostname', '');
+        $this->RegisterPropertyInteger('port', '80');
+
+        $this->RegisterPropertyString('user', '');
+        $this->RegisterPropertyString('password', '');
+
+        $this->RegisterMessage(0, IPS_KERNELMESSAGE);
+    }
+
+    // Inspired by module SymconTest/HookServe
+    public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
+    {
+        parent::MessageSink($TimeStamp, $SenderID, $Message, $Data);
+
+        if ($Message == IPS_KERNELMESSAGE && $Data[0] == KR_READY) {
+            $this->RegisterHook('/hook/MediolaServer');
+        }
+    }
+
+    public function ApplyChanges()
+    {
+        parent::ApplyChanges();
+
+        $vpos = 0;
+
+        $this->MaintainVariable('Data', $this->Translate('Data for CallBack'), vtString, '', $vpos++, true);
+
+        $this->SetStatus(102);
+
+        // Inspired by module SymconTest/HookServe
+        // Only call this in READY state. On startup the WebHook instance might not be available yet
+        if (IPS_GetKernelRunlevel() == KR_READY) {
+            $this->RegisterHook('/hook/Luftdaten');
+        }
+    }
+
+    public function GetConfigurationForm()
+    {
+        $formElements = [];
+        $formElements[] = ['type' => 'ValidationTextBox', 'name' => 'hostname', 'caption' => 'Hostname'];
+        $formElements[] = ['type' => 'Label', 'label' => 'Port ist 80 for the Gateway and typically 8088 for the NEO Server'];
+        $formElements[] = ['type' => 'NumberSpinner', 'name' => 'port', 'caption' => 'Port'];
+
+        $formElements[] = ['type' => 'ValidationTextBox', 'name' => 'user', 'caption' => 'User'];
+        $formElements[] = ['type' => 'ValidationTextBox', 'name' => 'password', 'caption' => 'Password'];
+
+        $formActions = [];
+        $formActions[] = ['type' => 'Button', 'label' => 'Verify Configuration', 'onClick' => 'MediolaServer_VerifyConfiguration($id);'];
+        $formActions[] = ['type' => 'Label', 'label' => '____________________________________________________________________________________________________'];
+        $formActions[] = [
+                            'type'    => 'Button',
+                            'caption' => 'Module description',
+                            'onClick' => 'echo "https://github.com/demel42/IPSymconMediolaServer/blob/master/README.md";'
+                        ];
+
+        $formStatus = [];
+        $formStatus[] = ['code' => '101', 'icon' => 'inactive', 'caption' => 'Instance getting created'];
+        $formStatus[] = ['code' => '102', 'icon' => 'active', 'caption' => 'Instance is active'];
+        $formStatus[] = ['code' => '104', 'icon' => 'inactive', 'caption' => 'Instance is inactive'];
+        $formStatus[] = ['code' => '201', 'icon' => 'inactive (invalid configuration)', 'caption' => 'Instance is inactive (invalid configuration)'];
+
+        return json_encode(['elements' => $formElements, 'actions' => $formActions, 'status' => $formStatus]);
+    }
+
+    public function VerifyConfiguration()
+    {
+    }
+
+    // Inspired from module SymconTest/HookServe
+    protected function ProcessHookData()
+    {
+        $this->SendDebug('WebHook SERVER', print_r($_SERVER, true), 0);
+
+        $root = realpath(__DIR__);
+        $uri = $_SERVER['REQUEST_URI'];
+        if (substr($uri, -1) == '/') {
+            http_response_code(404);
+            die('File not found!');
+        }
+        if ($uri == '/hook/MediolaServer') {
+            $data = file_get_contents('php://input');
+            $jdata = json_decode($data, true);
+            if ($jdata == '') {
+                echo 'malformed data: ' . $data;
+                $this->SendDebug(__FUNCTION__, 'malformed data: ' . $data, 0);
+                return;
+            }
+			// DOIT 
+            return;
+        }
+        http_response_code(404);
+        die('File not found!');
+    }
+}
---
+++ README.md
@@ -0,0 +1,74 @@
+# IPSymconMediolaGateway
+
+[![IPS-Version](https://img.shields.io/badge/Symcon_Version-5.0-red.svg)](https://www.symcon.de/service/dokumentation/entwicklerbereich/sdk-tools/sdk-php/)
+![Module-Version](https://img.shields.io/badge/Modul_Version-0.9-blue.svg)
+![Code](https://img.shields.io/badge/Code-PHP-blue.svg)
+[![License](https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-green.svg)](https://creativecommons.org/licenses/by-nc-sa/4.0/)
+[![StyleCI](https://github.styleci.io/repos/126683101/shield?branch=master)](https://github.styleci.io/repos/xxxxxxx)
+
+## Dokumentation
+
+**Inhaltsverzeichnis**
+
+1. [Funktionsumfang](#1-funktionsumfang)
+2. [Voraussetzungen](#2-voraussetzungen)
+3. [Installation](#3-installation)
+4. [Funktionsreferenz](#4-funktionsreferenz)
+5. [Konfiguration](#5-konfiguration)
+6. [Anhang](#6-anhang)
+7. [Versions-Historie](#7-versions-historie)
+
+## 1. Funktionsumfang
+
+## 2. Voraussetzungen
+
+ - IP-Symcon ab Version 5
+
+## 3. Installation
+
+Die Konsole von IP-Symcon öffnen. Im Objektbaum unter Kerninstanzen die Instanz __*Modules*__ durch einen doppelten Mausklick öffnen.
+
+In der _Modules_ Instanz rechts oben auf den Button __*Hinzufügen*__ drücken.
+
+In dem sich öffnenden Fenster folgende URL hinzufügen:
+
+`https://github.com/demel42/IPSymconMediolaGateway.git`
+
+und mit _OK_ bestätigen.
+
+Anschließend erscheint ein Eintrag für das Modul in der Liste der Instanz _Modules_
+
+### Einrichtung in IPS
+
+In IP-Symcon nun _Instanz hinzufügen_ (_CTRL+1_) auswählen unter der Kategorie, unter der man die Instanz hinzufügen will, und Hersteller _Mediola_ und als Gerät _Gateway V5/V5+/NEO Server_ auswählen.
+
+## 4. Funktionsreferenz
+
+### zentrale Funktion
+
+`boolean xxxxx(integer $InstanzID)`<br>
+
+## 5. Konfiguration:
+
+### Variablen
+
+| Eigenschaft                          | Typ      | Standardwert    | Beschreibung |
+| :----------------------------------: | :-----:  | :-------------: | :----------------------------------------------------------------------------------------------------------: |
+
+#### Schaltflächen
+
+| Bezeichnung                  | Beschreibung |
+| :--------------------------: | :-------------------------------------------------: |
+
+## 6. Anhang
+
+GUIDs
+
+- Modul: `{C0BD3A9B-D600-4B78-B9CC-173AC2819CE5}`
+- Instanzen:
+  - MediolaGateway: `{3525077B-2902-459F-BFA9-E9F4F18B4C0B}`
+
+## 7. Versions-Historie
+
+- 0.9 @ 17.09.2018 16:58<br>
+  Initiale Version
